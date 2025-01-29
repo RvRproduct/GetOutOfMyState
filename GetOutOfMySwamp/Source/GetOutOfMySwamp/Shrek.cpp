c72388c366666d99c72b281e5d5c028c4dd34d55
Roberto Valentino Reynoso <68643525+RvRproduct@users.noreply.github.com>
2025-01-28 20:08:57 -0700

    Shrek

---
+++ GetOutOfMySwamp/Source/GetOutOfMySwamp/Shrek.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Shrek.h"
+
+// Sets default values
+AShrek::AShrek()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AShrek::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AShrek::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AShrek::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
